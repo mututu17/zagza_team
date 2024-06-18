a6f0c2a20e5bfa88cdeb6a10d62074819f065d9e
SangYeon Yoon <yskite09@gmail.com>
2024-06-18 19:13:45 +0900

    Feat: 사용방법 팝업 수정

--- js/Modalpopup.js
+++ js/Modalpopup.js
@@ -11,3 +11,17 @@ document.addEventListener("DOMContentLoaded", function () {
         modal.style.display = "none";
     });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+    const openModalButton = document.getElementById("filter-manual-btn");
+    const closeModalButton = document.getElementById("mobile_closeModalButton");
+    const modal = document.getElementById("mobile_myModal");
+
+    openModalButton.addEventListener("click", function () {
+        modal.style.display = "block";
+    });
+
+    closeModalButton.addEventListener("click", function () {
+        modal.style.display = "none";
+    });
+});
